4379754b32c62995e5c9bdb176ccae716f459fb3
David Anson <david@dlaa.me>
2024-09-19 14:46:02 -0700

    Delete leftover heading comment, unused focus-mode.css file, and reformat background.js very slightly (fixes #25, fixes #23).

--- background.js
+++ background.js
@@ -1,25 +1,19 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+const extensions = [
+    'https://portal.azure.com',
+    'https://ms.portal.azure.com',
+    'https://rc.portal.azure.com',
+    'https://preview.portal.azure.com'
+];
+
 async function loadScriptAndStartCloaking(tabId, frameIds, allFrames) {
-  await chrome.scripting.executeScript({
-    target: { tabId, allFrames, frameIds },
-    files: ['cloak.js']
-  });
-  await chrome.scripting.executeScript({
-    target: { tabId, allFrames, frameIds },
-    function: () => { cloakTextAndStartObserving(); },
-  });
+    await chrome.scripting.executeScript({
+        target: { tabId, allFrames, frameIds },
+        files: ['cloak.js']
+    });
+    await chrome.scripting.executeScript({
+        target: { tabId, allFrames, frameIds },
+        function: () => { cloakTextAndStartObserving(); },
+    });
 }
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -28,13 +22,11 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-const extensions = ['https://portal.azure.com', 'https://ms.portal.azure.com', 'https://rc.portal.azure.com', 'https://preview.portal.azure.com'];
-
 chrome.webNavigation.onCompleted.addListener(async (details) => {
     const currentState = await chrome.action.getBadgeText({ tabId: details.tabId });
 
     if (currentState === 'ON' && details.frameId !== 0) { // This is an iframe
-        loadScriptAndStartCloaking(details.tabId, [details.frameId], false);  
+        loadScriptAndStartCloaking(details.tabId, [details.frameId], false);
     }
 }, { url: [{ urlMatches: 'https://*/*' }] });  // Matches all https URLs
 
