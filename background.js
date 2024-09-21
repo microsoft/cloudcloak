95d1b2214e115efc71df3c1794ca5fc75853804a
Guruprasad Airy <gurua@microsoft.com>
2024-09-20 17:38:20 -0700

    Update the status of the toolbar icon based on the domain

--- background.js
+++ background.js
@@ -1,69 +1,100 @@
 const extensions = [
-    'https://portal.azure.com',
-    'https://ms.portal.azure.com',
-    'https://rc.portal.azure.com',
-    'https://preview.portal.azure.com', 
-    'https://entra.microsoft.com', 
-    'https://intune.microsoft.com', 
-    'https://ai.azure.com', 
-    'https://admin.microsoft.com',
-    'https://sip.security.microsoft.com', 
-    'https://purview.microsoft.com',
-    'https://make.powerapps.com', 
-    'https://make.preview.powerapps.com',
-    'https://msazure.visualstudio.com', 
-    'https://github.com',
-    'https://copilotstudio.microsoft.com', 
-    'https://copilotstudio.preview.microsoft.com'
+  'https://portal.azure.com',
+  'https://ms.portal.azure.com',
+  'https://rc.portal.azure.com',
+  'https://preview.portal.azure.com',
+  'https://entra.microsoft.com',
+  'https://intune.microsoft.com',
+  'https://ai.azure.com',
+  'https://admin.microsoft.com',
+  'https://sip.security.microsoft.com',
+  'https://purview.microsoft.com',
+  'https://make.powerapps.com',
+  'https://make.preview.powerapps.com',
+  'https://msazure.visualstudio.com',
+  'https://github.com',
+  'https://copilotstudio.microsoft.com',
+  'https://copilotstudio.preview.microsoft.com'
 ];
 
+const currentState = { enabled: false };
+
 async function loadScriptAndStartCloaking(tabId, frameIds, allFrames) {
-    await chrome.scripting.executeScript({
-        target: { tabId, allFrames, frameIds },
-        files: ['cloak.js']
-    });
-    await chrome.scripting.executeScript({
-        target: { tabId, allFrames, frameIds },
-        function: () => { cloakTextAndStartObserving(); },
-    });
+  await chrome.scripting.executeScript({
+    target: { tabId, allFrames, frameIds },
+    files: ['cloak.js']
+  });
+  await chrome.scripting.executeScript({
+    target: { tabId, allFrames, frameIds },
+    function: () => { cloakTextAndStartObserving(); },
+  });
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: 'OFF'
+function updateBadgeAndExecute(tabId) {
+  // Set the badge text, color and tooltip
+  chrome.action.setBadgeText({
+    tabId: tabId,
+    text: currentState.enabled ? "ON" : "OFF"
+  });
+
+  chrome.action.setBadgeBackgroundColor({
+    tabId: tabId,
+    color: currentState.enabled ? "#00FF00" : "#FF0000"
+  });
+
+  chrome.action.setTitle({
+    tabId: tabId,
+    title: "Cloud Cloak (Available on this page)"
+  });
+
+  // If enabled is true, blur the text. Otherwise, unblur it
+  if (currentState.enabled) {
+    // Blur the text
+    loadScriptAndStartCloaking(tabId, null, true);
+  } else {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId, allFrames: true },
+      function: () => { unCloakTextAndStopObserving(); },
     });
+  }
+}
+
+function eventHandler(tabUrl, tabId) {
+  if (!tabUrl || !extensions.some((url) => tabUrl.startsWith(url))) {
+    chrome.action.setBadgeText({
+      tabId: tabId,
+      text: ""
+    });
+
+    chrome.action.setTitle({
+      tabId: tabId,
+      title: "Cloud Cloak (Unsupported on this page)"
+    });
+  } else {
+    updateBadgeAndExecute(tabId);
+  }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => { eventHandler(tab.url, tabId); });
+
+chrome.tabs.onActivated.addListener(() => {
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    eventHandler(tabs[0].url, tabs[0].id);
+  });
 });
 
 chrome.webNavigation.onCompleted.addListener(async (details) => {
-    const currentState = await chrome.action.getBadgeText({ tabId: details.tabId });
+  const currentState = await chrome.action.getBadgeText({ tabId: details.tabId });
 
-    if (currentState === 'ON' && details.frameId !== 0) { // This is an iframe
-        loadScriptAndStartCloaking(details.tabId, [details.frameId], false);
-    }
+  if (currentState === 'ON' && details.frameId !== 0) { // This is an iframe
+    loadScriptAndStartCloaking(details.tabId, [details.frameId], false);
+  }
 }, { url: [{ urlMatches: 'https://*/*' }] });  // Matches all https URLs
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
-    if (extensions.some((url) => tab.url.startsWith(url))) {
-        // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState
-        });
-
-        if (nextState === 'ON') {
-            // Blur the text
-            loadScriptAndStartCloaking(tab.id, null, true);
-        } else if (nextState === 'OFF') {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id, allFrames: true },
-                function: () => { unCloakTextAndStopObserving(); },
-            });
-        }
-    }
+  if (extensions.some((url) => tab.url.startsWith(url))) {
+    currentState.enabled = !currentState.enabled;
+    updateBadgeAndExecute(tab.id);
+  }
 });
